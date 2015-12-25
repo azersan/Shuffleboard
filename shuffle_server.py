bf14fb4450c92fd9e2cb6fd6beb8a3234592f84a
Tony Azersky <anthony.azersky@gmail.com>
2015-12-25 13:41:04 -0500

    Get rid of a bunch of unused stuff

--- shuffle_server.py
+++ shuffle_server.py
@@ -6,7 +6,6 @@
 import os
 import urlparse
 import time
-from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from flask.ext.mysqldb import MySQL
@@ -23,10 +22,6 @@ mysql = MySQL()
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DEBUG=True,
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default',
     MYSQL_USER = url.username,
     MYSQL_PASSWORD = url.password,
     MYSQL_DB = url.path[1:],
@@ -40,46 +35,9 @@ app.config.update(dict(
     MYSQL_SQL_MODE=None,
     MYSQL_CURSORCLASS=None
 ))
-# app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
-
-# def connect_db():
-#     """Connects to the specific database."""
-#     rv = sqlite3.connect(app.config['DATABASE'])
-#     rv.row_factory = sqlite3.Row
-#     return rv
-#
-#
-# def init_db():
-#     """Initializes the database."""
-#     with closing(connect_db()) as db:
-#         with app.open_resource('schema.sql', mode='r') as f:
-#             db.cursor().executescript(f.read())
-#         db.commit()
-
-
-# @app.cli.command('initdb')
-# def initdb_command():
-#     """Creates the database tables."""
-#     init_db()
-#     print('Initialized the database.')
-
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    # if not hasattr(g, 'sqlite_db'):
-    #     g.sqlite_db = connect_db()
-    # return g.sqlite_db
     return mysql.connection
-
-
-# @app.teardown_appcontext
-# def close_db(error):
-#     """Closes the database again at the end of the request."""
-#     if hasattr(g, 'sqlite_db'):
-#         g.sqlite_db.close()
 
 @app.route('/')
 def get_main_page():
